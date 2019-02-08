9bc9bbd019f4934ffc5bff8eb32bdc15de204746
scizorman <tueda1207@gmail.com>
2019-02-08 17:35:10 +0900

    [change] Rename

--- usecases/interactor/wfs.go
+++ usecases/interactor/wfs.go
@@ -7,24 +7,24 @@ import (
 
 // WFSInteractor is the interactor for MAO controller.
 type WFSInteractor struct {
-	Config     ports.Config
-	WFSHandler domain.WFSHandler
+	Config  ports.Config
+	Handler domain.WFSHandler
 }
 
 // NewWFSInteractor creates the new interactor for the WFS usecase.
 func NewWFSInteractor(conf ports.Config, handler domain.WFSHandler) *WFSInteractor {
 	return &WFSInteractor{
-		Config:     conf,
-		WFSHandler: handler,
+		Config:  conf,
+		Handler: handler,
 	}
 }
 
 // Start starts MAO wavefront sensor.
 func (i *WFSInteractor) Start() error {
-	if err := i.WFSHandler.Initialize(i.Config); err != nil {
+	if err := i.Handler.Initialize(i.Config); err != nil {
 		return err
 	}
-	if err := i.WFSHandler.Start(); err != nil {
+	if err := i.Handler.Start(); err != nil {
 		return err
 	}
 	return nil
@@ -32,10 +32,10 @@ func (i *WFSInteractor) Start() error {
 
 // Halt stops MAO wavefront sensor.
 func (i *WFSInteractor) Halt() error {
-	if err := i.WFSHandler.Halt(); err != nil {
+	if err := i.Handler.Halt(); err != nil {
 		return err
 	}
-	if err := i.WFSHandler.Finalize(); err != nil {
+	if err := i.Handler.Finalize(); err != nil {
 		return err
 	}
 	return nil
