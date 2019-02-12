58811da8e622cc483c827d890977236b69d28275
scizorman <tueda1207@gmail.com>
2019-02-12 14:48:28 +0900

    [add] Formatter of MAO configure (interface)

--- usecases/ports/config.go
+++ usecases/ports/config.go
@@ -2,6 +2,21 @@ package ports
 
 // ConfigPort is the configure for MAO.
 type ConfigPort interface {
-	Correlator(args ...interface{})
-	Switch(args ...interface{})
+	FormatConfig(corrConf *CorrelatorConfig, swConf *SwitchConfig) (*WFSConfig, error)
+}
+
+// WFSConfig represents the configure for MAO.
+type WFSConfig struct {
+	Correlator CorrelatorConfig
+	Switch     SwitchConfig
+}
+
+// CorrelatorConfig represents the configure for the correlator of MAO.
+// NOTE: TODO
+type CorrelatorConfig struct {
+}
+
+// SwitchConfig represents the configure for the switch of MAO.
+// NOTE: TODO
+type SwitchConfig struct {
 }
