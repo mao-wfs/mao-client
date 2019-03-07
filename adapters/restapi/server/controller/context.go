1b0efeef6c60bfa6c205fd321ddde58543124fe2
scizorman <tueda1207@gmail.com>
2019-03-08 01:35:35 +0900

    [remove] Unused files
    I deleted because it was an incorrect implementation of
    "Clean Architecture".

--- adapters/restapi/server/controller/context.go
+++
@@ -1,8 +0,0 @@
-package controller
-
-// Context represents the context of the current HTTP request.
-type Context interface {
-	Bind(i interface{}) error
-	String(code int, s string) error
-	JSON(code int, i interface{}) error
-}
