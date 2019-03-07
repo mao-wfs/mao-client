c8e471a64a948eb8bac2b0ad3b8755abcddefa72
scizorman <tueda1207@gmail.com>
2019-03-08 01:12:19 +0900

    [add] Input port for WFS

---
+++ usecases/input/wfs.go
@@ -0,0 +1,29 @@
+package input
+
+import (
+	"github.com/mao-wfs/maoctrl/domain"
+)
+
+// WFSInputPort is the input port for MAO-WFS.
+type WFSInputPort interface {
+	InitializeWFS(req *InitRequest) error
+	FinalizeWFS() error
+	StartWFS(req *StartRequest) error
+	HaltWFS() error
+}
+
+// InitRequest represents the request to initialize MAO-WFS.
+type InitRequest struct {
+	WFSConf *domain.WFSConf
+}
+
+// GetWFSConf gets "WFSConf".
+func (req *InitRequest) GetWFSConf() *domain.WFSConf { return req.WFSConf }
+
+// StartRequest represents the request to start MAO-WFS.
+type StartRequest struct {
+	WFSTime *domain.WFSTime
+}
+
+// GetWFSTime gets "WFSTime".
+func (req *StartRequest) GetWFSTime() *domain.WFSTime { return req.WFSTime }
