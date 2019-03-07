1f2eee796f6277e508e74554b0323b773672f3dc
scizorman <tueda1207@gmail.com>
2019-03-08 01:09:15 +0900

    [change] Define arguments of the handler
    It is difficult to implement the handler in the adapters layer
    when we define "WFSConf" and "WFSTime" in the usecases layer.
    So we defined them in the domain layer.

--- domain/wfs.go
+++ domain/wfs.go
@@ -1,9 +1,38 @@
 package domain
 
-// WFSHandler represents the domain handler for MAO wavefront sensor.
+import (
+	"time"
+)
+
+// WFSHandler represents the domain handler for MAO-WFS.
 type WFSHandler interface {
-	Initialize(args ...interface{}) error
+	Initialize(conf *WFSConf) error
 	Finalize() error
-	Start(t ...interface{}) error
+	Start(t *WFSTime) error
 	Halt() error
 }
+
+// WFSConf represents the configure of MAO-WFS.
+type WFSConf struct {
+	SwitchOrder SwitchOrder
+}
+
+// SwitchOrder is the order of the switch to enable.
+type SwitchOrder []SwitchPort
+
+// SwitchPort is the port of the switch to enable.
+type SwitchPort string
+
+// GetSwitchOrder gets "SwitchOrder".
+func (conf *WFSConf) GetSwitchOrder() SwitchOrder { return conf.SwitchOrder }
+
+// WFSTime represents the time of MAO-WFS.
+type WFSTime struct {
+	SensingTime SensingTime
+}
+
+// SensingTime is the sensing time of MAO-WFS.
+type SensingTime time.Duration
+
+// GetSensingTime gets "SensingTime".
+func (t *WFSTime) GetSensingTime() SensingTime { return t.SensingTime }
