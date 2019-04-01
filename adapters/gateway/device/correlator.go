50ff7d4d3b49a7deae14d583ecc19a81a718c157
scizorman <tueda1207@gmail.com>
2019-04-01 19:10:14 +0900

    [add] Device gateway in the adapters layer

---
+++ adapters/gateway/device/correlator.go
@@ -0,0 +1,105 @@
+package device
+
+import (
+	"regexp"
+	"strconv"
+
+	"golang.org/x/xerrors"
+
+	"github.com/mao-wfs/maoctrl/domain"
+)
+
+// Result codes of the correlator operations.
+const (
+	resultOK int = iota
+	resultReset
+	resultNotExecutable
+	resultInvalidArgs
+	resultUnknownError
+	_
+	resultConflict
+	resultInvalidKwd
+)
+
+// Error messages of the correlator operations.
+const (
+	msgErrNotExecutable = "cannot executed due to the differense of the operation mode"
+	msgErrInvalidArgs   = "invalid arguments"
+	msgErrUnknown       = "error while executing this command"
+	msgErrConflict      = "cannot executed due to a contradiction or conflicting commands"
+	msgErrInvalidKwd    = "invalid keyword"
+)
+
+// CorrelatorHandler is the handler of the correlator of MAO-WFS.
+type CorrelatorHandler struct {
+	Client
+}
+
+// NewCorrelatorHandler returns the handler of the correlator.
+func NewCorrelatorHandler(clt Client) *CorrelatorHandler {
+	return &CorrelatorHandler{Client: clt}
+}
+
+// Initialize initializes the correlator of MAO-WFS.
+func (h *CorrelatorHandler) Initialize() error {
+	return nil
+}
+
+// Finalize finalizes the correlator of MAO-WFS.
+func (h *CorrelatorHandler) Finalize() error {
+	msg := "reset=system;"
+	buf, err := h.Query(msg, defaultBufSize)
+	if err != nil {
+		return xerrors.Errorf("error in Finalize(): %w", err)
+	}
+	res := string(buf)
+	if err := h.checkResult(res); err != nil {
+		return xerrors.Errorf("error in Finalize(): %w", err)
+	}
+	return nil
+}
+
+// Start starts the correlator of MAO-WFS.
+func (h *CorrelatorHandler) Start(t domain.SensingTime) error {
+	return nil
+}
+
+// Halt halts the correlator of MAO-WFS.
+func (h *CorrelatorHandler) Halt() error {
+	msg := "corr_stop=2002001010000;"
+	buf, err := h.Query(msg, defaultBufSize)
+	if err != nil {
+		return xerrors.Errorf("error in Halt(): %w", err)
+	}
+	res := string(buf)
+	if err := h.checkResult(res); err != nil {
+		return xerrors.Errorf("error in Halt(): %w", err)
+	}
+	return nil
+}
+
+// checkResult checks the result code of the correlator operations.
+func (h *CorrelatorHandler) checkResult(res string) error {
+	resCode := h.getResultCode(res)
+	switch resCode {
+	case resultNotExecutable:
+		return xerrors.New(msgErrNotExecutable)
+	case resultInvalidArgs:
+		return xerrors.New(msgErrInvalidArgs)
+	case resultUnknownError:
+		return xerrors.New(msgErrUnknown)
+	case resultConflict:
+		return xerrors.New(msgErrConflict)
+	case resultInvalidKwd:
+		return xerrors.New(msgErrInvalidKwd)
+	default:
+		return nil
+	}
+}
+
+// getResultCode gets the result code of the correlator operations.
+func (h *CorrelatorHandler) getResultCode(msg string) int {
+	re := regexp.MustCompile(`[0-9]`)
+	resCode, _ := strconv.Atoi(re.FindString(msg))
+	return resCode
+}
---
+++ adapters/gateway/device/wfs_handler.go
@@ -0,0 +1,60 @@
+package device
+
+import (
+	"golang.org/x/xerrors"
+
+	"github.com/mao-wfs/maoctrl/domain"
+)
+
+// WFSHandler is the handler of MAO-WFS.
+type WFSHandler struct {
+	Correlator *CorrelatorHandler
+	Switch     *SwitchHandler
+}
+
+// NewWFSHandler returns the handler of MAO-WFS.
+func NewWFSHandler(corrClt, swClt Client) *WFSHandler {
+	return &WFSHandler{
+		Correlator: NewCorrelatorHandler(corrClt),
+		Switch:     NewSwitchHandler(swClt),
+	}
+}
+
+// Initialize initializes MAO-WFS.
+func (h *WFSHandler) Initialize(conf *domain.WFSConf) error {
+	if err := h.Correlator.Initialize(); err != nil {
+		return xerrors.Errorf("error in correlator: %w", err)
+	}
+	swOrder := conf.GetSwitchOrder()
+	if err := h.Switch.Initialize(swOrder); err != nil {
+		return xerrors.Errorf("error in switch: %w", err)
+	}
+	return nil
+}
+
+// Finalize finalizes MAO-WFS.
+func (h *WFSHandler) Finalize() error {
+	if err := h.Correlator.Finalize(); err != nil {
+		return xerrors.Errorf("error in correlator: %w", err)
+	}
+	if err := h.Switch.Finalize(); err != nil {
+		return xerrors.Errorf("error in switch: %w", err)
+	}
+	return nil
+}
+
+// Start starts MAO-WFS.
+func (h *WFSHandler) Start(t *domain.WFSTime) error {
+	return nil
+}
+
+// Halt halts MAO-WFS.
+func (h *WFSHandler) Halt() error {
+	if err := h.Correlator.Halt(); err != nil {
+		return xerrors.Errorf("error in correlator: %w", err)
+	}
+	if err := h.Switch.Finalize(); err != nil {
+		return xerrors.Errorf("error in switch: %w", err)
+	}
+	return nil
+}
